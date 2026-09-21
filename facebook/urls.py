29a046fd08a059c44ea2a3bab83ab0d843a9a743
Yero-des <yero_zc@hotmail.com>
2026-09-21 12:28:30 -0500

    Corrección a css en sidebar Implementación de funcionalidad de seguimiento

--- facebook/urls.py
+++ facebook/urls.py
@@ -5,5 +5,6 @@ app_name = 'facebook'
 urlpatterns = [
     path('', views.IndexListView.as_view(), name='index'),
     path('post/create', views.PostCreateView.as_view(), name='create-post'),
-    path('user/detail/<int:pk>', views.UserDetailView.as_view(), name="detail-user")
+    path('user/detail/<int:pk>', views.UserDetailView.as_view(), name="detail-user"),
+    path('follow/<int:user_id>', views.FollowView.as_view(), name='follow-user')
 ]
--- facebook/views.py
+++ facebook/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.http import HttpResponseForbidden
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
-from .models import Post
+from .models import Post, Follow
 
 User = get_user_model()
 
@@ -34,16 +34,43 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         context["current_view"] = 'create-post'
         return context
     
-    
 class UserDetailView(DetailView):
     model = User
     context_object_name = 'user'
     template_name = 'facebook/detail_user.html'
     
     def get_context_data(self, **kwargs):
+
         context = super().get_context_data(**kwargs)
         context["user_posts"] = self.object.posts.all()
         context["is_user_detail"] = True
         context["current_view"] = 'users'
+        context['is_following'] = Follow.objects.filter(
+            followed_user=self.object,
+            following_user=self.request.user,
+        ).exists()        
+        
         return context
+
+class FollowView(LoginRequiredMixin, View):
+    
+    def post(self, request, user_id):
+        
+        user_to_follow = get_object_or_404(User, id=user_id)
+        
+        follow = Follow.objects.filter(
+            followed_user=user_to_follow,
+            following_user=request.user,
+        )
+        
+        # Si el seguimiento existe se elimina
+        if follow.exists():
+            follow.delete()
+        else:
+            Follow.objects.create(
+                following_user=request.user,
+                followed_user=user_to_follow
+            )
+        
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     
